b4bd403318cdf624363ad08a3f041f83d1017591
Kirill <kirill.tsay@gmail.com>
2020-12-03 09:46:15 +0600

    Add topPadding variable

--- Toast/Toast.swift
+++ Toast/Toast.swift
@@ -624,6 +624,11 @@ public struct ToastStyle {
     public var verticalPadding: CGFloat = 10.0
     
     /**
+     Top Padding of Toast container view
+     */
+    public var topPadding: CGFloat = UIView.safeTopAreaHeight + 30
+    
+    /**
      The corner radius. Default is 10.0.
     */
     public var cornerRadius: CGFloat = 10.0;
@@ -795,7 +800,7 @@ public enum ToastPosition {
     case bottom
     
     fileprivate func centerPoint(forToast toast: UIView, inSuperview superview: UIView) -> CGPoint {
-        let topPadding: CGFloat = UIView.safeTopAreaHeight + 32
+        let topPadding: CGFloat = ToastManager.shared.style.topPadding
         let bottomPadding: CGFloat = ToastManager.shared.style.verticalPadding + superview.csSafeAreaInsets.bottom
         
         switch self {
